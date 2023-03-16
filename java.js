d64929b6ae9d99e5ca645e456113f4c6711c1f55
Israel-W-C-Costa <127996839+Israel-W-C-Costa@users.noreply.github.com>
2023-03-16 17:22:45 -0300

    seriesflix

---
+++ java.js
@@ -0,0 +1,4 @@
+let listaSerie=["https://br.web.img2.acsta.net/pictures/19/07/10/20/01/2331295.jpg","https://s3-sa-east-1.amazonaws.com/catalogodasartes/obra_13109129.jpg", "https://br.web.img3.acsta.net/pictures/22/11/30/19/53/5856320.jpg"];
+
+for (var i=0;i<listaSerie.length;i++) 
+{document.write("<img src="+ listaSerie[i]+">")};
